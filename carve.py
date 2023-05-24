75a6751cf0aad5346fc5316ebef9644d666aa0a6
xKomorebi <theoazach@gmail.com>
2023-05-23 21:30:27 -0400

    Moved Tests, Syntax is being defined within a py.

--- carve.py
+++ carve.py
@@ -1,45 +1,57 @@
 import tkinter as tk
-from tkinter import filedialog, colorchooser
+from tkinter import filedialog, colorchooser, simpledialog
 import json
 import os
 import datetime
 import re
+from syntax import c_syntax_highlighting
 
-LOG_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), f"carve_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.log")
-SETTINGS_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "settings.carve")
+LOG_FILE = f"carve_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
+SETTINGS_FILE = "settings.carve"
 
-c_syntax_highlighting = {
-    ".c": {
-        "keywords": [
-            "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum",
-            "extern", "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed",
-            "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"
-        ],
-        "operators": [
-            "+", "-", "*", "/", "%", "=", "==", "!=", ">", "<", ">=", "<=", "&&", "||", "!", "&", "|", "~",
-            "^", "<<", ">>"
-        ],
-        "comments": ["//", "/*", "*/"],
-        "keyword_color": "blue",
-        "operator_color": "purple",
-        "comment_color": "green"
-    },
-    ".h": {
-        "keywords": [
-            "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum",
-            "extern", "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed",
-            "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"
-        ],
-        "operators": [
-            "+", "-", "*", "/", "%", "=", "==", "!=", ">", "<", ">=", "<=", "&&", "||", "!", "&", "|", "~",
-            "^", "<<", ">>"
-        ],
-        "comments": ["//", "/*", "*/"],
-        "keyword_color": "blue",
-        "operator_color": "purple",
-        "comment_color": "green"
-    }
-}
+class TextFunctions:
+    def __init__(self, text_widget):
+        self.text_widget = text_widget
+        self.undo_stack = []
+        self.redo_stack = []
+
+        self.text_widget.bind("<Control-z>", self.undo)
+        self.text_widget.bind("<Control-y>", self.redo)
+        self.text_widget.bind("<Control-f>", self.find)
+        self.text_widget.bind("<Control-x>", self.cut_selected)
+
+    def undo(self, event=None):
+        if self.undo_stack:
+            text_state = self.undo_stack.pop()
+            self.redo_stack.append(self.text_widget.get("1.0", tk.END))
+            self.text_widget.delete("1.0", tk.END)
+            self.text_widget.insert("1.0", text_state)
+
+    def redo(self, event=None):
+        if self.redo_stack:
+            text_state = self.redo_stack.pop()
+            self.undo_stack.append(self.text_widget.get("1.0", tk.END))
+            self.text_widget.delete("1.0", tk.END)
+            self.text_widget.insert("1.0", text_state)
+
+    def find(self, event=None):
+        self.text_widget.tag_remove("match", "1.0", tk.END)
+        search_term = simpledialog.askstring("Find", "Enter text:")
+        if search_term:
+            matches = 0
+            start_pos = "1.0"
+            while True:
+                start_pos = self.text_widget.search(search_term, start_pos, stopindex=tk.END)
+                if not start_pos:
+                    break
+                end_pos = f"{start_pos}+{len(search_term)}c"
+                self.text_widget.tag_add("match", start_pos, end_pos)
+                matches += 1
+                self.text_widget.tag_config("match", foreground=self.match_foreground, background=self.match_background)
+                start_pos = end_pos
+
+    def cut_selected(self, event=None):
+        self.text_widget.event_generate("<<Cut>>")
 
 class TextEditor:
     def __init__(self):
@@ -47,11 +59,10 @@ class TextEditor:
         self.root.title("Simple Text Editor")
 
         self.current_file = None
-        self.window_count = 0
         self.text_clones = []
         self.font_name = tk.StringVar()  # Initialize as StringVar
         self.font_size = tk.IntVar(value=12)
-        self.syntax_highlighting_colors = {}  # Initialize syntax highlighting colors dictionary
+        self.syntax_highlighting_colors = {}
 
         self.keyword_color = tk.StringVar(value="blue")
         self.operator_color = tk.StringVar(value="purple")
@@ -80,11 +91,30 @@ class TextEditor:
         self.button_frame = tk.Frame(self.root)
         self.button_frame.pack()
 
-        self.root.bind("<Control-t>", lambda event: self.open_new_window)
+        self.root.bind("<Control-t>", lambda event: self.open_new_window())
+        self.root.bind("<Control-z>", self.undo)
+        self.root.bind("<Control-y>", self.redo)
+        self.root.bind("<Control-x>", self.cut_selected)
+        self.root.bind("<Control-f>", self.find)
 
         self.load_settings()
 
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+        self.settings = self.load_settings()
+        self.text_functions = TextFunctions(self.text)
+
+    def undo(self, event=None):
+        self.text_functions.undo()
+
+    def redo(self, event=None):
+        self.text_functions.redo()
+
+    def find(self, event=None):
+        self.text_functions.find()
+
+    def cut_selected(self, event=None):
+        self.text_functions.cut_selected()
 
     def log_event(self, event):
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -93,7 +123,7 @@ class TextEditor:
             log.write(log_entry)
 
     def open_file(self):
-        filepath = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt"), ("C Files", "*.c")])
+        filepath = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt"), ("C Files", "*.c"), ("Lua Files", "*.lua")])
         if filepath:
             self.current_file = filepath
             self.log_event(f"Opened file: {self.current_file}")
@@ -113,9 +143,9 @@ class TextEditor:
             with open(self.current_file, "w") as file:
                 file.write(self.text.get(1.0, tk.END))
         else:
-            filepath = filedialog.asksaveasfilename(defaultextension=".c", filetypes=[("C Files", "*.c"), ("Text Files", "*.txt")])
+            filepath = filedialog.asksaveasfilename(defaultextension=".c", filetypes=[("C Files", "*.c"), ("Text Files", "*.txt"), ("Lua Files", "*.lua")])
             if filepath:
-                if not filepath.endswith((".c", ".txt")):
+                if not filepath.endswith((".c", ".txt", ".lua")):
                     filepath += ".c"
                 self.current_file = filepath
                 self.log_event(f"Saved file: {self.current_file}")
@@ -123,9 +153,9 @@ class TextEditor:
                     file.write(self.text.get(1.0, tk.END))
 
     def save_as_file(self):
-        filepath = filedialog.asksaveasfilename(defaultextension=".c", filetypes=[("C Files", "*.c"), ("Text Files", "*.txt")])
+        filepath = filedialog.asksaveasfilename(defaultextension=".c", filetypes=[("C Files", "*.c"), ("Text Files", "*.txt"), ("Lua Files", "*.lua")])
         if filepath:
-            if not filepath.endswith((".c", ".txt")):
+            if not filepath.endswith((".c", ".txt", ".lua")):
                 filepath += ".c"
             self.current_file = filepath
             self.log_event(f"Saved file as: {self.current_file}")
@@ -133,11 +163,11 @@ class TextEditor:
                 file.write(self.text.get(1.0, tk.END))
 
     def on_closing(self):
-        # Log the file that was closed
         if self.current_file:
             self.log_event(f"Closed file: {self.current_file}")
         else:
             self.log_event("Closed new file")
+        self.root.destroy()
 
     def open_settings(self):
         settings_window = tk.Toplevel(self.root)
@@ -172,14 +202,15 @@ class TextEditor:
                 json.dump(settings, settings_file, indent=4)
 
         def load_settings():
+            settings = {}
             if os.path.exists(SETTINGS_FILE):
                 with open(SETTINGS_FILE, "r") as settings_file:
                     settings = json.load(settings_file)
-                    self.font_name.set(settings.get("font_name", ""))
-                    self.font_size.set(settings.get("font_size", 12))
-                    self.text.config(bg=settings.get("background_color", "white"))
-                    self.text.config(fg=settings.get("font_color", "black"))
-                    self.log_event("Loaded settings")
+            self.font_name.set(settings.get("font_name", ""))
+            self.font_size.set(settings.get("font_size", 12))
+            self.text.config(bg=settings.get("background_color", "white"))
+            self.text.config(fg=settings.get("font_color", "black"))
+            self.log_event("Loaded settings")
 
         font_label = tk.Label(settings_window, text="Font:")
         font_label.pack()
@@ -211,8 +242,6 @@ class TextEditor:
         settings_window = tk.Toplevel(self.root)
         settings_window.title("Syntax Highlighting Settings")
 
-        self.syntax_highlighting_colors = {}
-
         def choose_color(item):
             color = colorchooser.askcolor(title=f"Choose Color for {item}")[1]
             if color:
@@ -229,19 +258,11 @@ class TextEditor:
             if os.path.exists(SETTINGS_FILE):
                 with open(SETTINGS_FILE, "r") as settings_file:
                     settings = json.load(settings_file)
-                    self.font_name.set(settings.get("font_name", ""))
-                    self.font_size.set(settings.get("font_size", 12))
-                    self.text.config(bg=settings.get("background_color", "white"))
-                    self.text.config(fg=settings.get("font_color", "black"))
-                    syntax_highlighting_colors = settings.get("syntax_highlighting_colors", {})
-                    self.keyword_color.set(syntax_highlighting_colors.get("keyword", "blue") or "blue")
-                    self.operator_color.set(syntax_highlighting_colors.get("operator", "purple") or "purple")
-                    self.comment_color.set(syntax_highlighting_colors.get("comment", "green") or "green")
+                    self.syntax_highlighting_colors = settings.get("syntax_highlighting_colors", {})
+                    self.keyword_color.set(self.syntax_highlighting_colors.get("keyword", "blue"))
+                    self.operator_color.set(self.syntax_highlighting_colors.get("operator", "purple"))
+                    self.comment_color.set(self.syntax_highlighting_colors.get("comment", "green"))
                     self.log_event("Loaded settings")
-            else:
-                self.keyword_color.set("blue")
-                self.operator_color.set("purple")
-                self.comment_color.set("green")
 
         for item, color in self.syntax_highlighting_colors.items():
             button = tk.Button(settings_window, text=f"Choose Color for {item}", command=lambda i=item: choose_color(i))
@@ -255,11 +276,14 @@ class TextEditor:
     def apply_syntax_highlighting(self):
         _, extension = os.path.splitext(self.current_file)
         syntax_rules = c_syntax_highlighting.get(extension, {})
-        for item, keywords in syntax_rules.items():
+
+        for item, color in syntax_rules.items():
             tags = item + "_tag"
-            color = self.syntax_highlighting_colors.get(item, "black")
-            self.text.tag_configure(tags, foreground=color)
-            for keyword in keywords:
+            self.text.tag_configure(tags, foreground=self.syntax_highlighting_colors.get(item, "black"))
+
+            self.text.tag_remove(tags, "1.0", tk.END)
+
+            for keyword in syntax_rules[item]:
                 pattern = r"\b" + re.escape(keyword) + r"\b"
                 start = "1.0"
                 while True:
@@ -271,31 +295,27 @@ class TextEditor:
                     start = end
 
     def load_settings(self):
+        settings = {}
         if os.path.exists(SETTINGS_FILE):
             with open(SETTINGS_FILE, "r") as settings_file:
                 settings = json.load(settings_file)
-                self.font_name.set(settings.get("font_name", ""))
-                self.font_size.set(settings.get("font_size", 12))
-                self.text.config(bg=settings.get("background_color", "white"))
-                self.text.config(fg=settings.get("font_color", "black"))
-                self.syntax_highlighting_colors = settings.get("syntax_highlighting_colors", {})
-                self.log_event("Loaded settings")
+        self.font_name.set(settings.get("font_name", ""))
+        self.font_size.set(settings.get("font_size", 12))
+        self.text.config(bg=settings.get("background_color", "white"))
+        self.text.config(fg=settings.get("font_color", "black"))
+        self.syntax_highlighting_colors = settings.get("syntax_highlighting_colors", {})
+        self.log_event("Loaded settings")
 
     def open_new_window(self):
-        if self.window_count < 2:
-            self.window_count += 1
+        if len(self.text_clones) < 2:
             new_window = tk.Toplevel(self.root)
-            new_window.title(f"Editor {self.window_count}")
+            new_window.title(f"Editor {len(self.text_clones) + 1}")
 
             text_clone = tk.Text(new_window, wrap=tk.WORD)
             text_clone.pack(fill=tk.BOTH, expand=True)
             text_clone.config(font=(self.font_name.get(), self.font_size.get()))
 
             self.text_clones.append(text_clone)
-
-    def on_closing(self):
-        self.log_event("Application stopped")
-        self.root.destroy()
 
     def run(self):
         self.root.mainloop()
---
+++ syntax.py
@@ -0,0 +1,45 @@
+c_syntax_highlighting = {
+    ".c": {
+        "keywords": [
+            "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum",
+            "extern", "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed",
+            "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"
+        ],
+        "operators": [
+            "+", "-", "*", "/", "%", "=", "==", "!=", ">", "<", ">=", "<=", "&&", "||", "!", "&", "|", "~",
+            "^", "<<", ">>"
+        ],
+        "comments": ["//", "/*", "*/"],
+        "keyword_color": "blue",
+        "operator_color": "purple",
+        "comment_color": "green"
+    },
+    ".h": {
+        "keywords": [
+            "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum",
+            "extern", "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed",
+            "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"
+        ],
+        "operators": [
+            "+", "-", "*", "/", "%", "=", "==", "!=", ">", "<", ">=", "<=", "&&", "||", "!", "&", "|", "~",
+            "^", "<<", ">>"
+        ],
+        "comments": ["//", "/*", "*/"],
+        "keyword_color": "blue",
+        "operator_color": "purple",
+        "comment_color": "green"
+    },
+    ".lua": {
+        "keywords": [
+            "and", "break", "do", "else", "elseif", "end", "false", "for", "function", "goto", "if", "in", "local",
+            "nil", "not", "or", "repeat", "return", "then", "true", "until", "while"
+        ],
+        "operators": [
+            "+", "-", "*", "/", "%", "=", "==", "~=", "<", ">", "<=", ">="
+        ],
+        "comments": ["--"],
+        "keyword_color": "blue",
+        "operator_color": "purple",
+        "comment_color": "green"
+    }
+}
